af81615a77f274c4a487a61b4faf1ffaeab1b78f
hanamaruGames <nao@hanamaru.games>
2024-01-11 13:21:23 +0900

    アニメーションファイル追加

---
+++ SwordBout/project/Source/animation.cpp
@@ -0,0 +1,62 @@
+#include "animation.h"
+#include "../Library/myDxLib.h"
+
+Animation::Animation()
+{
+	attachIndex = -1;
+	frameSpeed = 0.5f;
+}
+
+Animation::Animation(GameObject* parent)
+{
+	attachIndex = -1;
+	frameSpeed = 0.5f;
+}
+
+Animation::~Animation()
+{
+	if (attachIndex >= 0)
+		MV1DetachAnim(baseModel, attachIndex);
+}
+
+void Animation::Update()
+{
+	if (attachIndex < 0)
+		return;
+
+	frame += frameSpeed;
+	if (loopMode) {
+		if (frame>=maxFrame) {
+			frame -= maxFrame;
+		}
+	} else {
+		if (frame>maxFrame) {
+			frame = maxFrame;
+		}
+	}
+	MV1SetAttachAnimTime(baseModel, attachIndex, frame);
+}
+
+void Animation::SetModel(int model)
+{
+	baseModel = model;
+}
+
+void Animation::Play(int animModel, bool loop)
+{
+	if (animModel == curAnim)
+		return;
+
+	curAnim = animModel;
+	if (attachIndex>=0) {
+		MV1DetachAnim(baseModel, attachIndex);
+		attachIndex = -1;
+	}
+	attachIndex = MV1AttachAnim(baseModel, 0,
+									animModel);
+	loopMode = loop;
+	frame = 0;
+	maxFrame = MV1GetAttachAnimTotalTime(
+						baseModel, attachIndex);
+	MV1SetAttachAnimTime(baseModel, attachIndex, 0);
+}
---
+++ SwordBout/project/Source/animation.h
@@ -0,0 +1,34 @@
+#pragma once
+// animation.h
+#include "component.h"
+
+class Animation : public Component {
+public:
+	Animation();
+	Animation(GameObject* parent);
+	~Animation();
+	void Update() override;
+	void SetModel(int model);
+	void Play(int animModel, bool loop=true);
+	float GetCurrentFrame() {return frame;}
+
+	/// <summary>
+	/// 再生速度を設定する
+	/// </summary>
+	/// <param name="speed">通常速度が1.0</param>
+	void SetFrameSpeed(float speed) {
+		frameSpeed = speed / 2.0f;
+	}
+	bool IsEnd() {
+		return !loopMode && frame >= maxFrame;
+	}
+
+private:
+	int baseModel;
+	int attachIndex;
+	float frame; // フレームを数える
+	float maxFrame;
+	float frameSpeed; // 再生速度
+	bool loopMode; // ループするか
+	int curAnim; // 今のアニメーション
+};
